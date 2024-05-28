919a2fed8e9be38977d9d1165f65ed3175cd9fb3
Imo <sangleksono41@gmail.com>
2024-05-28 21:38:17 +0700

    Pembaruan dengan animasi

--- register.php
+++ register.php
@@ -4,14 +4,12 @@ include "connection.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link
-			rel="icon"
-			href="asset/paw catto (new ver.).png"
-			type="image/png" />
+    <link rel="icon" href="asset/paw catto (new ver.).png" type="image/png" />
     <title>Sign Up Form by Colorlib</title>
 
     <!-- Font Icon -->
@@ -19,9 +17,10 @@ include "connection.php";
 
     <!-- Main css -->
     <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
 
+</head>
+
+<body>
     <div class="main">
 
         <!-- Sign up form -->
@@ -33,22 +32,23 @@ include "connection.php";
                         <form method="POST" class="register-form" id="register-form">
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="username" id="name" placeholder="Your Name" required/>
+                                <input type="text" name="username" id="name" placeholder="Your Name" required />
                             </div>
                             <div class="form-group">
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="password" id="pass" placeholder="Password" required/>
+                                <input type="password" name="password" id="pass" placeholder="Password" required />
                             </div>
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email" required/>
+                                <input type="email" name="email" id="email" placeholder="Your Email" required />
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all
+                                    statements in <a href="#" class="term-service">Terms of service</a></label>
                             </div>
                             <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register" />
                             </div>
                             <div class="alert alert-success" id="success" style="margin-top: 10px; display:none">
                                 <strong>Hi!</strong> Account registration successfully.
@@ -68,33 +68,34 @@ include "connection.php";
 
     </div>
 
-<?php
-if(isset($_POST["signup"]))  {
-    
-    $count=0;
-    $res=mysqli_query($link, "select * from registration where username='$_POST[username]'") or die (mysqli_error($link));
-    $count=mysqli_num_rows($res);
+    <?php
+    if (isset($_POST["signup"])) {
 
-    if($count>0) {
-        ?>
-        <script type="text/javascript">
-            document.getElementById("success").style.display="none";
-            document.getElementById("failure").style.display="block";
-        </script>
-        <?php
-    } else {
-        mysqli_query($link, "insert into registration values(NULL,'$_POST[username]','$_POST[password]','$_POST[email]')") or die (mysqli_error($link));
-        ?>
-        <script type="text/javascript">
-            window.location="login.php";
-        </script>
-        <?php
+        $count = 0;
+        $res = mysqli_query($link, "select * from registration where username='$_POST[username]'") or die(mysqli_error($link));
+        $count = mysqli_num_rows($res);
+
+        if ($count > 0) {
+            ?>
+            <script type="text/javascript">
+                document.getElementById("success").style.display = "none";
+                document.getElementById("failure").style.display = "block";
+            </script>
+            <?php
+        } else {
+            mysqli_query($link, "insert into registration values(NULL,'$_POST[username]','$_POST[password]','$_POST[email]')") or die(mysqli_error($link));
+            ?>
+            <script type="text/javascript">
+                window.location = "login.php";
+            </script>
+            <?php
+        }
     }
-}
-?>
+    ?>
 
     <!-- JS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
 </html>
